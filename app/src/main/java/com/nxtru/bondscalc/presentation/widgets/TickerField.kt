65c1e6068d9405e1a74ee234ce519fe0f33d54fc
vbogd <vbogd@users.noreply.github.com>
2022-10-31 23:32:09 +0300

    ticker field: dropdown works

--- app/src/main/java/com/nxtru/bondscalc/presentation/widgets/TickerField.kt
+++ app/src/main/java/com/nxtru/bondscalc/presentation/widgets/TickerField.kt
@@ -1,51 +1,96 @@
 package com.nxtru.bondscalc.presentation.widgets
 
 import android.view.KeyEvent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import com.nxtru.bondscalc.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TickerField(
     value: String,
+    onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier,
-    onDone: (String) -> Unit,
-    onValueChange: (String) -> Unit
+    tickers: List<String>,
+    onSearch: (String) -> Unit
 ) {
-    OutlinedTextField(
-        value = value,
-        onValueChange = onValueChange,
-        label = { Text(stringResource(R.string.ticker)) },
-        singleLine = true,
-        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-        keyboardActions = KeyboardActions(
-            onDone = { onDone(value) },
-        ),
-        modifier = modifier.onKeyEvent {
-            if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER){
-                onDone(value)
-                true
-            } else
-                false
+    var showMenu by remember { mutableStateOf(false) }
+    val loadingStr = stringResource(R.string.loading)
+    Box(
+        modifier = modifier
+    ) {
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            label = { Text(stringResource(R.string.ticker)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = { onSearch(value); showMenu = true },
+            ),
+            modifier = Modifier.fillMaxWidth().onKeyEvent {
+                if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER) {
+                    onSearch(value)
+                    showMenu = true
+                    true
+                } else
+                    false
+            }
+        )
+        DropdownMenu(
+            modifier = Modifier.fillMaxWidth(),
+            expanded = showMenu,
+            onDismissRequest = { showMenu = false },
+        ) {
+            tickers.forEach { label ->
+                DropdownMenuItem(
+                    text = { Text(text = label) },
+                    enabled = label != loadingStr,
+                    onClick = {
+                        println("item clicked: $label")
+                        onValueChange(label)
+                        showMenu = false
+                    }
+                )
+            }
         }
-    )
+    }
 }
 
 @Preview
 @Composable
 fun PreviewTickerField() {
-    TickerField("ОФЗ", Modifier.fillMaxWidth(), {
-        println(">>> enter1: $it")
-    }) {}
+    var ticker by remember { mutableStateOf("ОФЗ") }
+    var tickers by remember { mutableStateOf<List<String>>(emptyList()) }
+    val loadingStr = stringResource(R.string.loading)
+    val coroutineScope = rememberCoroutineScope()
+    TickerField(
+        value = ticker,
+        onValueChange = { ticker = it },
+        modifier = Modifier.fillMaxWidth(),
+        tickers = tickers,
+        onSearch = {
+            println("Searching '$it'...")
+            tickers = listOf(loadingStr)
+            coroutineScope.launch {
+                tickers = loadEmul(it)
+            }
+        }
+    )
+}
+
+suspend fun loadEmul(query: String): List<String> {
+    delay(1000L)
+    return listOf("$query-1", "$query-2", "$query-3")
 }
