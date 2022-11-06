1fafdbf25cf97f9fab1dfc51c892fc923645366d
vbogd <vbogd@users.noreply.github.com>
2022-11-06 22:28:32 +0300

    SearchScreen: add "Clear" button

--- app/src/main/java/com/nxtru/bondscalc/presentation/components/SearchScreen.kt
+++ app/src/main/java/com/nxtru/bondscalc/presentation/components/SearchScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -14,6 +16,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.nxtru.bondscalc.R
 import com.nxtru.bondscalc.domain.models.BriefBondInfo
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 data class SearchScreenUIState(
     val pattern: String = "",
@@ -28,25 +32,23 @@ private val padding = 8.dp
 fun SearchScreen(
     uiState: SearchScreenUIState,
     modifier: Modifier = Modifier,
-    onValueChange: (String) -> Unit,
-    onDoSearch: (String) -> Unit,
+    onUIStateChange: (SearchScreenUIState) -> Unit,
+    onSearch: (String) -> Unit,
     onSelected: (BriefBondInfo) -> Unit,
 ) {
     Column(
         modifier = modifier
     ) {
-        OutlinedTextField(
-            value = uiState.pattern,
-            onValueChange = onValueChange,
-            label = { Text(stringResource(R.string.ticker)) },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
-            keyboardActions = KeyboardActions(
-                onSearch = { onDoSearch(uiState.pattern) },
-            ),
+        TickerField(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = padding)
+                .padding(horizontal = padding),
+            value = uiState.pattern,
+            onSearch = onSearch,
+            onValueChange = { onUIStateChange(uiState.copy(pattern = it)) },
+            onClear = {
+                onUIStateChange(uiState.copy(pattern = "", tickers = emptyList()))
+            }
         )
         if (uiState.isSearching) {
             Box(
@@ -65,6 +67,35 @@ fun SearchScreen(
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun TickerField(
+    value: String,
+    modifier: Modifier = Modifier,
+    onValueChange: (String) -> Unit,
+    onSearch: (String) -> Unit,
+    onClear: () -> Unit,
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(stringResource(R.string.ticker)) },
+        singleLine = true,
+        trailingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Close,
+                contentDescription = null,
+                modifier = Modifier.clickable(onClick = onClear)
+            )
+        },
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        keyboardActions = KeyboardActions(
+            onSearch = { onSearch(value) },
+        ),
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -97,19 +128,29 @@ private fun BriefBondInfoCard(
 @Preview(showBackground = true)
 @Composable
 fun PreviewSearchScreen() {
+    val initial = SearchScreenUIState(
+        pattern = "26009",
+        isSearching = false,
+        tickers = listOf(
+            BriefBondInfo("SU26229RMFS3", "ОФЗ 26229", "RU000A100EG3"),
+            BriefBondInfo("SU29006RMFS2", "ОФЗ 29006", "RU000A0JV4L2"),
+            BriefBondInfo("SU29014RMFS6", "ОФЗ 29014", "RU000A101N52"),
+        )
+    )
+//    val uiStateFlow = MutableStateFlow(initial)
+//    val uiState: SearchScreenUIState by uiStateFlow.collectAsState()
+    var uiState by remember { mutableStateOf(initial) }
+    val coroutineScope = rememberCoroutineScope()
     SearchScreen(
-        uiState = SearchScreenUIState(
-            pattern = "26009",
-            isSearching = false,
-            tickers = listOf(
-                BriefBondInfo("SU26229RMFS3", "ОФЗ 26229", "RU000A100EG3"),
-                BriefBondInfo("SU29006RMFS2", "ОФЗ 29006", "RU000A0JV4L2"),
-                BriefBondInfo("SU29014RMFS6", "ОФЗ 29014", "RU000A101N52"),
-            ),
-        ),
+        uiState = uiState,
         modifier = Modifier.fillMaxSize(),
-        onValueChange = {},
-        onDoSearch = {},
-        onSelected = { println(">>> clicked: $it") }
+        onUIStateChange = {
+//            coroutineScope.launch {
+//                uiStateFlow.emit(it)
+//            }
+              uiState = it
+        },
+        onSearch = { uiState = uiState.copy(tickers = initial.tickers) },
+        onSelected = { uiState = uiState.copy(pattern = it.secId, tickers = emptyList()) }
     )
 }
