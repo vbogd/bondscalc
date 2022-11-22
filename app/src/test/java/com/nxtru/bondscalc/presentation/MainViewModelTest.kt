b42baf69b0562fe4cc0148c19d05610bd41940ce
vbogd <vbogd@users.noreply.github.com>
2022-11-22 13:17:48 +0300

    first tests for ViewModel

---
+++ app/src/test/java/com/nxtru/bondscalc/presentation/MainViewModelTest.kt
@@ -0,0 +1,146 @@
+package com.nxtru.bondscalc.presentation
+
+import androidx.arch.core.executor.ArchTaskExecutor
+import androidx.arch.core.executor.TaskExecutor
+import com.nxtru.bondscalc.domain.models.BondParams
+import com.nxtru.bondscalc.domain.usecase.LoadBondParamsUseCase
+import com.nxtru.bondscalc.domain.usecase.SaveBondParamsUseCase
+import com.nxtru.bondscalc.domain.usecase.bondinfo.LoadBondInfoUseCase
+import com.nxtru.bondscalc.domain.usecase.bondinfo.SearchTickersUseCase
+import com.nxtru.bondscalc.presentation.models.MainUIState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.never
+
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class MainViewModelTest {
+    private val saveBondParamsUseCase = mock<SaveBondParamsUseCase>()
+    private val loadBondParamsUseCase = mock<LoadBondParamsUseCase>()
+    private val searchTickersUseCase = mock<SearchTickersUseCase>()
+    private val loadBondInfoUseCase = mock<LoadBondInfoUseCase>()
+
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+
+    @BeforeEach
+    fun setUp() {
+        ArchTaskExecutor.getInstance().setDelegate(object : TaskExecutor() {
+            override fun executeOnDiskIO(runnable: Runnable) {
+                runnable.run()
+            }
+
+            override fun postToMainThread(runnable: Runnable) {
+                runnable.run()
+            }
+
+            override fun isMainThread(): Boolean {
+                return true
+            }
+        })
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @AfterEach
+    fun tearDown() {
+        Mockito.reset(
+            saveBondParamsUseCase,
+            loadBondParamsUseCase,
+            searchTickersUseCase,
+            loadBondInfoUseCase
+        )
+        ArchTaskExecutor.getInstance().setDelegate(null)
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun getUiStateFlow() {
+        val expected = createUIState(bondParams = testBondParams, calcResult = testCalcResult)
+        val actual = createViewModel(testBondParams).uiStateFlow.value
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    @Disabled
+    fun getErrorMessageCode() {
+        // TODO: implement
+    }
+
+    @Test
+    @Disabled
+    fun onUIStateChange() {
+        // TODO: implement
+    }
+
+    @Test
+    @Disabled
+    fun onSearchScreenSearch() {
+        // TODO: implement
+    }
+
+    @Test
+    @Disabled
+    fun onTickerSelectionDone() {
+        // TODO: implement
+    }
+
+    @Test
+    fun `onCalculatorScreenRefresh with empty secId`() = runTest {
+        createViewModel().onCalculatorScreenRefresh()
+
+        Mockito.verify(loadBondInfoUseCase, never()).invoke(any())
+    }
+
+    @Test
+    @Disabled
+    fun `onCalculatorScreenRefresh with non-empty secId`() {
+        // TODO: implement
+    }
+
+    private fun createViewModel(bondParams: BondParams = BondParams.EMPTY): MainViewModel {
+        Mockito.`when`(loadBondParamsUseCase.execute()).thenReturn(bondParams)
+        return MainViewModel(
+            saveBondParamsUseCase, loadBondParamsUseCase, searchTickersUseCase, loadBondInfoUseCase
+        )
+    }
+}
+
+private val testBondParams = BondParams(
+    ticker = "",
+    commission = "0.05",
+    tax = "13",
+    coupon = "5",
+    parValue = "1000",
+    buyPrice = "97.10",
+    buyDate = "26.03.2021",
+    sellPrice = "99",
+    sellDate = "06.05.2021",
+    tillMaturity = false
+)
+
+private val testCalcResult = BondCalcUIResult(
+    income="20,56 ₽",
+    ytm="18,83 %",
+    currentYield="4,48 %"
+)
+
+private fun createUIState(
+    bondParams: BondParams = BondParams.EMPTY,
+    calcResult: BondCalcUIResult = BondCalcUIResult.UNDEFINED,
+): MainUIState {
+    val res = MainUIState()
+    return res.copy(
+        calculatorScreenUIState = res.calculatorScreenUIState.copy(
+            bondParams = bondParams,
+            calcResult = calcResult
+        )
+    )
+}
