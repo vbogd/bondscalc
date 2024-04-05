62e3474e99f70cb33a278d4ede2b27ed0db3d7f6
vbogd <vbogd@users.noreply.github.com>
2024-04-05 19:12:37 +0300

    add link in app bar

--- app/src/main/java/com/nxtru/bondscalc/presentation/components/TopBar.kt
+++ app/src/main/java/com/nxtru/bondscalc/presentation/components/TopBar.kt
@@ -1,5 +1,7 @@
 package com.nxtru.bondscalc.presentation.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -10,7 +12,9 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -60,8 +64,28 @@ private fun CalculatorScreenTopBar(
     onSearch: () -> Unit,
     onRefresh: () -> Unit,
 ) {
+    val uriHandler = LocalUriHandler.current
+    val sponsorUrl = stringResource(R.string.sponsor_url)
+
     TopAppBar(
-        title = { Text(stringResource(R.string.calculator_screen_title)) },
+        title = {
+            Column {
+                Text(stringResource(R.string.calculator_screen_title))
+                Text(
+                    stringResource(R.string.sponsor),
+                    style = MaterialTheme.typography.titleMedium,
+                    color = LocalContentColor.current.copy(alpha = 0.5f),
+                    modifier = Modifier
+                        .clickable(
+                            // disable ripple effect
+                            indication = null,
+                            interactionSource = remember { MutableInteractionSource() }
+                        ) {
+                            uriHandler.openUri(sponsorUrl)
+                        }
+                )
+            }
+        },
         actions = {
             if (refreshAvailable) {
                 ClickableIcon(
